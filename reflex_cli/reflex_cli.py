e3ab05307fd493f175fb4716f5cfa70de30f7209
rjulian <richard@rjulian.net>
2020-01-25 13:29:56 -0800

    Add basic unit tests for reflex cli class

--- reflex_cli/reflex_cli.py
+++ reflex_cli/reflex_cli.py
@@ -8,12 +8,18 @@ CMD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), "commands")
 
 class ReflexCli(click.MultiCommand):
     """Class that handles subcommand setup."""
+    @staticmethod
+    def is_command_file(filename):
+        """Determines based on file name if command file."""
+        if filename.endswith(".py") and filename.startswith("cmd_"):
+            return True
+        return False
 
     def list_commands(self, context):
         command_files = []
         for filename in os.listdir(CMD_FOLDER):
-            if filename.endswith(".py") and filename.startswith("cmd_"):
-                command_files.append(filename[4:-3])
+                if self.is_command_file(filename):
+                    command_files.append(filename[4:-3])
         command_files.sort()
         return command_files
 
---
+++ test/test_reflex_cli.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+from reflex_cli import reflex_cli
+
+
+class ReflexCliTestCase(unittest.TestCase):
+    """Test class for the environment context of our CLI tool."""
+
+    def test_is_command_file(self):
+        """Test our defaults for the environment are sane."""
+        test_cli = reflex_cli.ReflexCli()
+        self.assertTrue(test_cli.is_command_file('cmd_test.py'))
+        self.assertFalse(test_cli.is_command_file('comd_test.py'))
