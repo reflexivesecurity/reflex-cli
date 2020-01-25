222b32c4ee69167ca568b7182afef80b0903a204
rjulian <richard@rjulian.net>
2020-01-25 12:49:49 -0800

    Basic config parser tests

---
+++ test/test_config_parser.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import MagicMock
+import os
+from reflex_cli import config_parser
+
+
+class ConfigParserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_config_file = os.path.abspath(os.path.join(
+            os.path.dirname(__file__),
+            "resources/reflex.yaml"
+            ))
+
+    def test_generate_config_returns_dictionary(self):
+        fresh_config = config_parser.ConfigParser(self.test_config_file)
+        self.assertTrue(isinstance(fresh_config.generate_config(), dict))
