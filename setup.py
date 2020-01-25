7f96741681c1d2879f8c16c8922c8edb2d9ea9f7
rjulian <richard@rjulian.net>
2020-01-25 13:37:02 -0800

    Add in jinja2 reqs and have gh actions install from freeze

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=["reflex_cli", "reflex_cli.commands"],
     package_data={"reflex_cli": ["templates/*"]},
     include_package_data=True,
-    install_requires=["click",],
+    install_requires=["click", "jinja2", "pyyaml"],
     entry_points="""
         [console_scripts]
         reflex=reflex_cli.cli:cli
