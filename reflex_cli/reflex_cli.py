6a5583348db01ff2930678843e102f3debebaa8a
rjulian <richard@rjulian.net>
2020-01-26 13:53:36 -0800

    Fix list_commands to actually capture command names

--- reflex_cli/reflex_cli.py
+++ reflex_cli/reflex_cli.py
@@ -24,7 +24,7 @@ class ReflexCli(click.MultiCommand):
         command_files = []
         for filename in os.listdir(CMD_FOLDER):
             if self.is_command_file(filename):
-                command_files.append(filename[4:-3])
+                command_files.append(filename[8:-3])
         command_files.sort()
         return command_files
 
