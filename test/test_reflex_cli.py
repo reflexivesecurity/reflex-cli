1716a601aef27253cf7cbc73346a31761ec9d0c3
rjulian <richard@rjulian.net>
2020-01-25 23:39:15 -0800

    Fix tests to conform to new command file structure

--- test/test_reflex_cli.py
+++ test/test_reflex_cli.py
@@ -9,5 +9,5 @@ class ReflexCliTestCase(unittest.TestCase):
     def test_is_command_file(self):
         """Test our defaults for the environment are sane."""
         test_cli = reflex_cli.ReflexCli()
-        self.assertTrue(test_cli.is_command_file('cmd_test.py'))
+        self.assertTrue(test_cli.is_command_file('command_test.py'))
         self.assertFalse(test_cli.is_command_file('comd_test.py'))
