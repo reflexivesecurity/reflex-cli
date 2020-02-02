49a5c2edce6d5627c098c04d97185cdbb39dc7ba
rjulian <richard@rjulian.net>
2020-02-02 13:55:40 -0800

    Add in quick implementation of show command

---
+++ reflex_cli/commands/command_show.py
@@ -0,0 +1,17 @@
+"""
+Show command is used for discovery of detective measures that you may
+want to include in your reflex configuration.
+"""
+import logging
+import click
+from reflex_cli.measure_discoverer import MeasureDiscoverer
+
+LOGGER = logging.getLogger("reflex_cli")
+
+
+@click.command("show", short_help="Shows possible reflex measures.")
+def cli():
+    """CLI entrypoint for show command."""
+    discoverer = MeasureDiscoverer()
+    discoverer.collect_measures()
+    discoverer.display_discovered_measures()
---
+++ reflex_cli/measure_discoverer.py
@@ -0,0 +1,53 @@
+"""Holder of template generation logic"""
+import os
+import logging
+import github
+
+LOGGER = logging.getLogger("reflex_cli")
+
+
+class MeasureDiscoverer:
+    """Discovers measures by querying github remotes."""
+
+    def __init__(self):
+        self.discovered_measures = {}
+        self.github_organizations = ["cloudmitigator"]
+
+        if os.environ.get("REFLEX_GITHUB_TOKEN"):
+            self.github_client = github.Github(
+                os.environ.get("REFLEX_GITHUB_TOKEN")
+            )
+        else:
+            self.github_client = github.Github()
+
+    def collect_measures(self):
+        """Iterates over github org and collects repos that match rules."""
+        for organization in self.github_organizations:
+            LOGGER.debug("Collecting repos for %s", organization)
+            org_api = self.github_client.get_organization(organization)
+            rule_repositories = self.filter_reflex_repos(org_api)
+            self.discovered_measures[organization] = rule_repositories
+
+    def filter_reflex_repos(self, org_api):
+        """Determines if a repo name matches the rule convention naming."""
+        filtered_repos = []
+        for repo in org_api.get_repos():
+            LOGGER.debug("Checking repo for %s", repo.name)
+            if self.is_rule_repository(repo.name):
+                filtered_repos.append(repo)
+        return filtered_repos
+
+    @staticmethod
+    def is_rule_repository(repo_name):
+        """Deterministic element for a repository to match a rule."""
+        return repo_name.startswith("reflex-aws")
+
+    def display_discovered_measures(self):
+        """Method outputs measure information in a usable fashion"""
+        for organization in self.github_organizations:
+            LOGGER.info(
+                "Rules discovered in %s Github organization.", organization
+            )
+            LOGGER.info("-------------------------------------------")
+            for rule_repo in self.discovered_measures[organization]:
+                LOGGER.info("%s: %s", rule_repo.name, rule_repo.description)
