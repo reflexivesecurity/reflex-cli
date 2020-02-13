60e02ac885fac7a2e59975e5fa38067596cb0c09
Max Anderson <maxwell.c.anderson@gmail.com>
2020-02-12 16:54:41 -0800

    Extract Github API calls to separate class. Begin fixing/adding tests for GitHub code refactor

--- test/test_measure_discoverer.py
+++ test/test_measure_discoverer.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from reflex_cli.measure_discoverer import MeasureDiscoverer
 
@@ -9,32 +9,28 @@ class MeasureDiscovererTestCase(unittest.TestCase):
 
     def setUp(self):
         self.discoverer = MeasureDiscoverer()
-        self.org_api_mock = MagicMock()
+        self.git_repos_mock = MagicMock()
         self.repo_mock1 = MagicMock()
         self.repo_mock1.name = "reflex-aws-test"
         self.repo_mock2 = MagicMock()
         self.repo_mock2.name = "reflex-test"
-        self.org_api_mock.get_repos.return_value = [
-            self.repo_mock1,
-            self.repo_mock2,
-        ]
-        self.discoverer.github_client.get_organization = MagicMock(
-            return_value=self.org_api_mock
-        )
 
     def test_is_rule_repository(self):
         self.assertTrue(self.discoverer.is_rule_repository("reflex-aws-test"))
         self.assertFalse(self.discoverer.is_rule_repository("aws-test"))
 
     def test_filter_reflex_repos(self):
-        filtered_repos = self.discoverer.filter_reflex_repos(self.org_api_mock)
+        repos = [self.repo_mock1, self.repo_mock2]
+        filtered_repos = self.discoverer.filter_reflex_repos(repos)
         self.assertEqual(filtered_repos[0].name, "reflex-aws-test")
         self.assertEqual(len(filtered_repos), 1)
 
-    def test_collect_measures(self):
-        self.assertEqual(self.discoverer.discovered_measures, {})
+    @patch("reflex_cli.reflex_github.ReflexGithub.get_repos")
+    def test_collect_measures(self, mock_get_repos):
+        mock_get_repos.return_value = [self.repo_mock1, self.repo_mock2]
+        self.assertEqual(self.discoverer.discovered_measures, [])
 
-        mocked_measures = {"cloudmitigator": [self.repo_mock1]}
+        mocked_measures = [self.repo_mock1]
         self.discoverer.collect_measures()
 
         self.assertEqual(self.discoverer.discovered_measures, mocked_measures)
