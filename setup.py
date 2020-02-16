0fbc275cf49778c9ac37590d2c71c5e63869656c
Max Anderson <maxwell.c.anderson@gmail.com>
2020-02-16 11:59:54 -0800

    Don't use hardcoded version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="reflex-cli",
-    version="0.0.1",
+    version=f"{os.environ['VERSION']}",
     packages=["reflex_cli", "reflex_cli.commands"],
     package_data={"reflex_cli": ["templates/*"]},
     include_package_data=True,
