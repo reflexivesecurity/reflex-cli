0d17563505ce19c1b64918b4e83867393f375c41
rjulian <richard@rjulian.net>
2020-03-02 20:16:50 -0800

    Format terraform better

--- reflex_cli/templates/aws-enforce.tf
+++ reflex_cli/templates/aws-enforce.tf
@@ -1,5 +1,5 @@
 module "{{module_name}}" {
-  source        = "git@github.com:{{github_org}}/{{template_name}}.git?ref={{version}}"
-  sns_topic_arn = module.central-sns-topic.arn
+  source            = "git@github.com:{{github_org}}/{{template_name}}.git?ref={{version}}"
+  sns_topic_arn     = module.central-sns-topic.arn
   reflex_kms_key_id = module.reflex-kms-key.key_id
 }
