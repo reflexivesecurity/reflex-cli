914748677c824c36906bf1cebd6fa96ab40fd147
Max Anderson <maxwell.c.anderson@gmail.com>
2020-03-06 11:33:48 -0800

    Minor template fix

--- reflex_cli/templates/aws-rule.tf
+++ reflex_cli/templates/aws-rule.tf
@@ -2,7 +2,7 @@ module "{{module_name}}" {
   source            = "git@github.com:{{github_org}}/{{template_name}}.git?ref={{version}}"
   sns_topic_arn     = module.central-sns-topic.arn
   reflex_kms_key_id = module.reflex-kms-key.key_id
-{% if mode -%}
+{% if mode is not none -%}
   mode              = {{mode}}
 {%- endif %}
 }
