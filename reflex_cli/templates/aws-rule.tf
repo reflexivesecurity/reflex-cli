b355a448134122cd685236757650881309bd3f7e
Max Anderson <maxwell.c.anderson@gmail.com>
2020-03-31 08:57:11 -0700

    Make reflex build support configuration block being dict

--- reflex_cli/templates/aws-rule.tf
+++ reflex_cli/templates/aws-rule.tf
@@ -2,22 +2,21 @@ module "{{module_name}}" {
   source            = "git::https://github.com/{{github_org}}/{{template_name}}.git?ref={{version}}"
   sns_topic_arn     = module.central-sns-topic.arn
   reflex_kms_key_id = module.reflex-kms-key.key_id
+
 {%- if configuration -%}
-  {%- for configurable in configuration -%}
-    {%- if configurable is mapping %}
-      {%- for key, value in configurable.items() %}
-        {%- if value is mapping %}
-    {{key}}              = {
+  {%- for key, value in configuration.items() -%}
+    {%- if value is mapping %}
+
+  {{key}} = {
       {%- for map_key, map_value in value.items() %}
-      {{map_key}}: {{map_value}}
+    {{map_key}}: {{map_value}}
       {%- endfor %}
-    }
-        {%- else %}
-    {{key}}              = "{{value}}"
-        {%- endif %}
-    {%- endfor %}
+  }
+
     {%- else %}
-    {{configurable}}   = ""
+
+  {{key}} = {{value}}
+
     {%- endif %}
   {%- endfor %}
 {%- endif %}
