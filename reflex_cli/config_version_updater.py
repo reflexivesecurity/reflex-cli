665b12e397ea50de0992ab2688f1b43ea4cc1847
Michael Schappacher <michael.schappacher@gmail.com>
2020-04-06 09:24:34 -0400

    Feature: add in new -r or --rule flag for command upgrade, to allow for updating a single rule by specifying it's name. Tested reflex upgrade with and without r, and also in cases where name was misspelled or did not exist.

--- reflex_cli/config_version_updater.py
+++ reflex_cli/config_version_updater.py
@@ -76,6 +76,7 @@ class ConfigVersionUpdater:
                     self._set_rule_value(rule.name, "version", remote_version)
 
     def compare_current_rule_version(self, rule_name):
+        """ Check single rule version and compare it to remote version."""
         remote_versions = self.gather_latest_remote_versions()
         for rule in self.current_config.rule_list:
             if rule.name == rule_name:
@@ -96,12 +97,11 @@ class ConfigVersionUpdater:
                     if self.user_input.verify_upgrade_interest():
                         self._set_rule_value(rule.name, "version", remote_version)
                     return
-                else:
-                    LOGGER.info(
-                        "%s rule does not have a new release.",
-                        rule.name
-                    )
-                    return
+                LOGGER.info(
+                    "%s rule does not have a new release.",
+                    rule.name
+                )
+                return
         LOGGER.info(
             "Rule with name %s does not exist, please check the spelling.",
             rule_name
