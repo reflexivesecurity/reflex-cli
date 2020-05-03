bc706b366296ce2337d1d7c9f8f7f18271eeaa20
rjulian <richard@rjulian.net>
2020-05-03 13:23:57 -0700

    Fix formatting, add in region specified output directory

--- reflex_cli/commands/command_region.py
+++ reflex_cli/commands/command_region.py
@@ -1,4 +1,7 @@
-"""Region command takes in configuration file outputs region forwarder infrastructure template"""
+"""
+Region command takes in configuration file outputs region
+forwarder infrastructure template
+"""
 # pylint: disable=line-too-long
 import logging
 import os
@@ -6,9 +9,10 @@ import os
 import click
 
 from reflex_cli.config_parser import ConfigParser
-from reflex_cli.template_generator import TemplateGenerator
+from reflex_cli.region_template_generator import RegionTemplateGenerator
 
 CONFIG_DEFAULT = os.path.abspath(os.path.join(os.getcwd(), "reflex.yaml"))
+OUTPUT_DEFAULT = os.path.abspath(os.path.join(os.getcwd(), "reflex_region"))
 LOGGER = logging.getLogger("reflex_cli")
 
 
@@ -16,9 +20,18 @@ LOGGER = logging.getLogger("reflex_cli")
     "region",
     short_help="Builds out tf files for region forwarder architecture.",
 )
-@click.option("-r", "--region", help="Region to forward to central region")
 @click.option(
-    "-q", "--central-sqs-arn", help="SQS Queue arn in central processing region"
+    "-o",
+    "--output",
+    type=click.Path(
+        exists=False, dir_okay=True, file_okay=False, resolve_path=True
+    ),
+    default=OUTPUT_DEFAULT,
+    help="Output directory for reflex",
+)
+@click.option("-r", "--region", help="Region to forward to central region.")
+@click.option(
+    "-q", "--central-sqs-arn", help="SQS queue arn in central region."
 )
 @click.option(
     "-c",
@@ -27,10 +40,20 @@ LOGGER = logging.getLogger("reflex_cli")
     default=CONFIG_DEFAULT,
     help="Configuration file for reflex",
 )
-def cli(region, central_sqs_arn, config):
+def cli(output, region, central_sqs_arn, config):
     """
-    Builds terraform output files for the multi-region support of reflex in AWS.
+    Builds terraform output for multi-region support of reflex in AWS.
 
-    Region will look at your configuration, generate the relevant templates and providers for deploying region forwarders.
+    Region will look at your configuration, generate the relevant templates
+    and providers for deploying region forwarders.
     """
     LOGGER.debug("Config file set to: %s", config)
+    configuration = ConfigParser(config)
+    configuration.parse_valid_config()
+    if output == OUTPUT_DEFAULT:
+        output = OUTPUT_DEFAULT + "_" + region.replace("-", "_")
+    generator = RegionTemplateGenerator(
+        configuration.raw_configuration, output, region, central_sqs_arn
+    )
+    LOGGER.info("Creating Terraform files...")
+    generator.create_templates()
