99aa8e4ba2b5d0ff0a4e9d522250818ab1be30ca
rjulian <richard@rjulian.net>
2020-06-10 21:56:44 -0700

    Fix reflex create for new multiregion infrastructure

---
+++ reflex_cli/templates/rule_templates/cwe.tf
@@ -0,0 +1,9 @@
+module "cwe" {
+  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe?ref={{ engine_version }}"
+  name        = "{{ rule_class_name }}"
+  description = "TODO: Provide rule description"
+
+  event_pattern = <<PATTERN
+# TODO: Provide event pattern
+PATTERN
+}
